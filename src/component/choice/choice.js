859774d1fba25e48f391ba8cea330d3fb3b3d0f3
Nstw <name.sp926@gmail.com>
2018-10-07 16:26:51 +0700

    choice v.1

--- src/component/choice/choice.js
+++ src/component/choice/choice.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import {
   Container,
-  Row,
   Card,
   CardGroup,
   CardImg,
@@ -13,35 +12,95 @@ import {
 import "../choice/style-choice.css";
 
 export default class Choice extends Component {
+  state = {
+    isMenu: true,
+    isPackage: false,
+    isSnack: false
+  };
+
+  constructor() {
+    super();
+
+    this.changeRenderToMenu = this.changeRenderToMenu.bind(this);
+    this.changeRenderToPackage = this.changeRenderToPackage.bind(this);
+    this.changeRenderToSnack = this.changeRenderToSnack.bind(this);
+    this.setActiveButton = this.setActiveButton.bind(this);
+  }
+
+  changeRenderToMenu() {
+    this.setState({
+      isMenu: true,
+      isPackage: false,
+      isSnack: false
+    });
+  }
+
+  changeRenderToPackage() {
+    this.setState({
+      isMenu: false,
+      isPackage: true,
+      isSnack: false
+    });
+  }
+
+  changeRenderToSnack() {
+    this.setState({
+      isMenu: false,
+      isPackage: false,
+      isSnack: true
+    });
+  }
+
+  setActiveButton(isButton) {
+    let color = "button";
+    color += isButton ? " btn--active" : "";
+    return color;
+  }
+
   render() {
     return (
-      <Container className="choice__block" fluid>
-        <Row className="justify-content-center">
+      <section className="choice__block" fluid>
+        <Container>
           <CardGroup className="cardgroup__choice">
             <Card className="card__choice">
               <CardImg src="../img/choice_1.png" className="cardimg__choice" />
               <CardBody>
-                <Button className="button btn--active">MENU</Button>
-                <CardText>This is a package</CardText>
+                <Button
+                  className={this.setActiveButton(this.state.isMenu)}
+                  onClick={this.changeRenderToMenu}
+                >
+                  MENU
+                </Button>
+                <CardText>This is a Menu</CardText>
               </CardBody>
             </Card>
             <Card className="card__choice">
               <CardImg src="../img/choice_2.png" className="cardimg__choice" />
               <CardBody>
-                <Button className="button">PACKAGE</Button>
+                <Button
+                  className={this.setActiveButton(this.state.isPackage)}
+                  onClick={this.changeRenderToPackage}
+                >
+                  PACKAGE
+                </Button>
                 <CardText>This is a Package</CardText>
               </CardBody>
             </Card>
             <Card className="card__choice">
               <CardImg src="../img/choice_3.png" className="cardimg__choice" />
               <CardBody>
-                <Button className="button">SNACK</Button>
+                <Button
+                  className={this.setActiveButton(this.state.isSnack)}
+                  onClick={this.changeRenderToSnack}
+                >
+                  SNACK
+                </Button>
                 <CardText>This is a Snack</CardText>
               </CardBody>
             </Card>
           </CardGroup>
-        </Row>
-      </Container>
+        </Container>
+      </section>
     );
   }
 }
